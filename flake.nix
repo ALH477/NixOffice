a4d4d11dec1cecb3fdaadf0a6eb72b46fc79238e
Asher <146795551+ALH477@users.noreply.github.com>
2025-10-24 17:43:35 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 DeMoD LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy...
+# (full MIT text, or just "Licensed under the MIT License. See LICENSE for details.")
 {
   description = "NixOffice: Optimized determinate NixOS config for production-grade office system";
 
